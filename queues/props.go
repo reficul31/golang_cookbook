0eee047da4a0241215ee34c6b3fbed3c8a3119ea
reficul31 <reficul31@gmail.com>
2018-02-17 07:42:05 +0530

    Adding properties for priority queue

--- queues/props.go
+++ queues/props.go
@@ -91,3 +91,26 @@ func (q *Queue) DeleteCircular() (int, error) {
 	}
 	return element, nil
 }
+
+// Function to insert into a priority queue
+func (q *Queue) InsertPriority(element int, priorityDecider func(int, int) bool) error {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if q.rear == q.capacity-1 {
+		return errors.New("Queue Capacity Full")
+	} else if q.rear == -1 && q.front == -1 {
+		q.front = q.front+1
+		q.rear = q.rear+1
+		q.arr[q.rear] = element
+		return nil
+	} else {
+		var i int
+		for i=q.rear+1 ; i>=q.front && !priorityDecider(q.arr[i-1], element); i-- {
+			q.arr[i] = q.arr[i-1]
+		}
+		q.arr[i] = element
+		q.rear = q.rear+1
+		return nil
+	}
+}
