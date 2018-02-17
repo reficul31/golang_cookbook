690c58e62e9bb833193bec67cfb0a078822bf51f
reficul31 <reficul31@gmail.com>
2018-02-17 13:31:19 +0530

    Adding properties for double ended queue

--- queues/props.go
+++ queues/props.go
@@ -114,3 +114,42 @@ func (q *Queue) InsertPriority(element int, priorityDecider func(int, int) bool)
 		return nil
 	}
 }
+
+// Function to insert from the front of the queue
+func (q *Queue) InsertFront(element int) error {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if q.front == -1 {
+		q.front = 0
+		q.rear = 0
+		q.arr[q.front] = element
+		return nil
+	} else if q.front == 0 {
+		return errors.New("Cannot insert from front")
+	} else {
+		q.front = q.front - 1
+		q.arr[q.front] = element
+		return nil
+	}
+}
+
+// Function to delete from the rear of the queue
+func (q *Queue) DeleteRear() (int, error) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	var element int
+
+	if q.front == -1 && q.rear == -1 {
+		return -1, errors.New("Queue Empty")
+	} else if q.front == q.rear {
+		element = q.arr[q.rear]
+		q.front = -1
+		q.rear = -1
+		return element, nil
+	} else {
+		element = q.arr[q.rear]
+		q.rear = q.rear - 1
+		return element, nil
+	}
+}
