a3b521cf5d57d6801bf2e621f2fe0df9845b97d7
reficul31 <reficul31@gmail.com>
2022-01-01 11:00:30 -0500

    Adding problems for contains duplicate and max profit

--- arrays/problems.go
+++ arrays/problems.go
@@ -1,5 +1,7 @@
 package arrays
 
+import "math"
+
 // Function to get indices in the array that add up to the sum
 // @arg nums - Array to search
 // @arg target - Target sum to achieve
@@ -16,4 +18,33 @@ func TwoSum(nums []int, target int) []int {
 		hash_map[num] = index
 	}
 	return []int{}
+}
+
+// Function to find the best time to buy and sell stock
+// @arg prices - Array of prices
+// @returns - Max profit from the array
+func MaxProfit(prices []int) int {
+	maxProfit, minPrice := 0, math.MaxInt16
+	for _, price := range prices {
+		if minPrice > price {
+			minPrice = price
+		}
+		if price-minPrice > maxProfit {
+			maxProfit = price - minPrice
+		}
+	}
+	return maxProfit
+}
+
+// Function to find if an array contains duplicates
+// @arg nums - Array of integers to find duplicates
+func ContainsDuplicate(nums []int) bool {
+	hash_map := make(map[int]bool)
+	for _, num := range nums {
+		if _, ok := hash_map[num]; ok {
+			return true
+		}
+		hash_map[num] = true
+	}
+	return false
 }
--- arrays/problems_test.go
+++ arrays/problems_test.go
@@ -15,3 +15,28 @@ func TestTwoSum(testCase *testing.T) {
 		testCase.Errorf("Two Sum Error: Function returned the wrong value")
 	}
 }
+
+// Test the two sum function
+func TestMaxProfit(testCase *testing.T) {
+	testCase.Log("To test the max profit from a list of stock prices")
+
+	prices := []int{7, 1, 5, 3, 6, 4}
+	solution := MaxProfit(prices)
+
+	if solution != 5 {
+		testCase.Errorf("Max Profit Error: Function returned the wrong value")
+	}
+}
+
+// Test the contains duplicates function
+func TestContainsDuplicate(testCase *testing.T) {
+	testCase.Log("To test the function to test the duplicates in an array")
+
+	if !ContainsDuplicate([]int{1, 2, 3, 1}) {
+		testCase.Errorf("Contains Duplicate Error: Function returned the wrong value")
+	}
+
+	if ContainsDuplicate([]int{1, 2, 3, 4}) {
+		testCase.Errorf("Contains Duplicate Error: Function returned the wrong value")
+	}
+}
